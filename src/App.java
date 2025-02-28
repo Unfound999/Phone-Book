d567f0f62e00fd18b59a63c01e15cd1749766c2a
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-02-28 15:07:28 -0800

    Created PhoneBookUI.java class. Added a call from App.java class to PhoneBookUI.java class.

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-    }
-}
+        PhoneBookUI.mainMenu();
+    } //  end of main method
+} //  end of App class
---
+++ src/PhoneBookUI.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PhoneBookUI {
+    public static void mainMenu() {
+
+    } //  end of mainMenu method
+
+    public static void viewAllEntries() {
+
+    } //  end of viewAllEntries method
+
+    public static void viewOneEntry() {
+
+    } //  end of viewOneEntry method
+
+    public static void addNewEntry() {
+
+    } //  end of addNewEntry method
+
+    public static void removeOneEntry() {
+
+    } //  end of removeOneEntry method
+} //  end of PhoneBookUI class
