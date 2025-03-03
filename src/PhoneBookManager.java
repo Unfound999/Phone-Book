0a7c4ff66e9ac917bb73588e11534f9a2c851103
Jackson Jenks <jacksocrod@gmail.com>
2025-03-03 15:28:22 -0800

    phonebook manager

--- src/PhoneBookManager.java
+++ src/PhoneBookManager.java
@@ -1,5 +1,27 @@
 public class PhoneBookManager {
 
-    
+    private final EntryNode firstNode;
+    private int length;
+
+    public void addEntry(String fNAme, String lName, String address, String phNum, int zip, String email) {
+
+        EntryNode currNode = firstNode;
+        while(currNode.getNextEntry() !=  null) {
+
+            currNode = currNode.getNextEntry();
+        }
+
+        EntryNode newEntryNode = new EntryNode(this.length++, fNAme, lName, address, phNum, zip, email);
+        currNode.setNextEntry(newEntryNode);
+
+    }
+
+    public PhoneBookManager(String fNAme, String lName, String address, String phNum, int zip, String email) {
+
+        this.length = 0;
+        this.firstNode = new EntryNode(this.length++, fNAme, lName, address, phNum, zip, email);
+    }
+
+
 
 }
