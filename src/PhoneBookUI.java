2b2c4ea5d3db4e70447a8cae369ab5e2221cbceb
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-03-04 16:18:12 -0800

    Added comments and more code formatting

--- src/PhoneBookUI.java
+++ src/PhoneBookUI.java
@@ -7,14 +7,16 @@
 import java.util.Scanner;
 
 public class PhoneBookUI {
+    //  Prompts user with main menu and runs methods based on user input
     public static void mainMenu() {
         Scanner userInput = new Scanner(System.in);
         String menuInput;
         boolean menuFlag = true;
         char menuOption;
         
-        ASCIIArt();
+        ASCIIArt(); //  prints ASCII art of phone book title
         
+        //  prints main menu prompt to user
         do {
             System.out.println("Please select one of the following options:\n" +
                             "(e) View all entries in phone book\n" +
@@ -25,6 +27,7 @@ public class PhoneBookUI {
             menuInput = userInput.next();
             menuOption = menuInput.toLowerCase().charAt(0);
 
+            //  calls methods based on user input & ensuring that the input is valid
             if (Character.toString(menuOption).matches("[evarq]")) {
                 switch(menuOption) {
                     case('e'): viewAllEntries(); break;
@@ -39,6 +42,7 @@ public class PhoneBookUI {
         } while (menuFlag);
     } //  end of mainMenu method
 
+    //  calls method from Manager class to obtain list of  entry names and phone numbers and prints them to user
     public static void viewAllEntries() {
         Scanner userInput = new Scanner(System.in);
         String menuInput;
@@ -49,19 +53,22 @@ public class PhoneBookUI {
         // entryNames = {Call method from Manager class here to get String of data, using a while loop, hasNext, and next to print each name one by one}
         Scanner names = new Scanner(entryNames);
 
-        // The following is just a hardcoded version of what UI would look like. This bit of code is temporary
-        printLine();
+        printLine(); //  prints solid line for user readability
         System.out.println("All entries: ");
-
+        
+        //  grabs names and phonenumbers from String and prints them to user
+        // The following is just a hardcoded version of what UI would look like. This bit of code is temporary
         while (names.hasNext()) {
             System.out.print(names.next() + " ");
             System.out.println(names.next());
         }
 
+        //  prompts user and either runs methods or quits back to main menu based on user input
         System.out.println("\nIf you would like to view a specific entry type \"v\" or to go back to the main menu, type \"q\"");
         menuInput = userInput.next();
         menuOption = menuInput.toLowerCase().charAt(0);
 
+        //  ensures that user enters a valid input of 'v' or 'q'
         do { 
             switch (menuOption) {
                 case('v'): viewOneEntry(); menuFlag = false; break;
@@ -75,6 +82,7 @@ public class PhoneBookUI {
         } while (menuFlag);
     } //  end of viewAllEntries method
 
+    //  checks if an entry exists based on user inputted data and if it does exist, print entry's information to user
     public static void viewOneEntry() {
         Scanner userInput = new Scanner(System.in);
         String entryFName, entryLName, phoneNum;
@@ -82,12 +90,15 @@ public class PhoneBookUI {
         boolean menuFlag = true;
         //  EntryNode entry = new EntryNode;
 
-        printLine();
+        printLine(); // prints solid line for user readability
 
+        /*  prompts user for specific entry's name and phonenumber, checks if that entry exists
+         *  if entry does exist, print entry's information to the user
+         *  If the user types "quit" at any point, quit back to main menu
+        */
         do {
             System.out.print("Please enter the first name of the entry you would like to see or type \"quit\" to quit: ");
             entryFName = userInput.next();
-
             if (entryFName.toLowerCase().matches("quit")) {
                 menuFlag = false;
             } else {
@@ -115,15 +126,16 @@ public class PhoneBookUI {
                 }
             }
         } while (menuFlag);
-        System.out.println("\n\n");
+        System.out.println("\n\n"); //  println for user readability
     } //  end of viewOneEntry method
 
+    //  prompts user for all data needed to create an entry and calls a Manager class method that creates entry with inputted data
     public static void addNewEntry() {
         Scanner userInput = new Scanner(System.in);
         Scanner addressInput = new Scanner(System.in);
         boolean nameFlag = false;
 
-        printLine();
+        printLine(); //  prints solid line for user readability
 
         System.out.print("Please enter the entry's first name: ");
         String entryFName = userInput.next();
@@ -143,8 +155,10 @@ public class PhoneBookUI {
         System.out.print("Please enter the entry's email address: ");
         String email = userInput.next();
 
+        //  checks if entry already exists
         // nameFlag = {method}; {Call method from manager class that checks if an object with that name exists in linkedlist}
 
+        //  if entry already exists, state so to user, but if entry does not exist, call Manager class method to create entry
         if (nameFlag) {
             System.out.println("User already exists");
         } else {
@@ -152,21 +166,25 @@ public class PhoneBookUI {
             System.out.println("User successfully created");
         }
 
-        System.out.println("\n\n");
+        System.out.println("\n\n"); //  println for user readability
     } //  end of addNewEntry method
 
+    //  checks if an entry exists based on user inputted data and if it does exist, call Manager method to remove entry
     public static void removeOneEntry() {
         Scanner userInput = new Scanner(System.in);
         String entryFName, entryLName, phoneNumber;
         boolean nameFlag = false;
         boolean menuFlag = true;
 
-        printLine();
+        printLine(); //  prints a solid line for user readability
 
+        /*  prompts user for specific entry's name and phonenumber, checks if that entry exists
+         *  if entry does exist, call Manager class method to remove entry
+         *  If the user types "quit" at any point, quit back to main menu
+        */
         do {
             System.out.print("Please enter the first name of the entry you would like to remove or type \"quit\" to quit: ");
             entryFName = userInput.next();
-
             if (entryFName.toLowerCase().matches("quit")) {
                 menuFlag = false;
             } else {
@@ -192,7 +210,7 @@ public class PhoneBookUI {
                 }
             }
         } while (menuFlag);
-        System.out.println("\n\n");
+        System.out.println("\n\n"); //  println for user readability
     } //  end of removeOneEntry method
 
     //  Prints ASCIIArt in main menu
@@ -215,6 +233,7 @@ public class PhoneBookUI {
                         "                                                                                                                ");
     } //  end of ASCIIArt method
 
+    //  prints a solid line to seperate different menus to improve User Interface
     public static void printLine() {
         System.out.println("\n\n***********************************************************************************");
     } //  end of printLine method
